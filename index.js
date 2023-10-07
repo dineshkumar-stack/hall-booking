1620725448b954756e08f8236a8519450e542bfc
dineshkumar-stack <stardinesh4@gmail.com>
2023-10-07 14:05:26 +0530

    added

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/create_room', createRoom);
-app.post('/book_room', bookRoom);
-app.get('/list_rooms', listRooms);
-app.get('/list_customers', listCustomers);
-app.get('/customer_history/:customer_name', customerBookingHistory)
+app.post('/api/create_room', createRoom);
+app.post('/api/book_room', bookRoom);
+app.get('/api/list_rooms', listRooms);
+app.get('/api/list_customers', listCustomers);
+app.get('/api/customer_history/:customer_name', customerBookingHistory)
 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT} Active`) });
